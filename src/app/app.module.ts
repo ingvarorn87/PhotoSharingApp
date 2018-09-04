455f2ed6ae23df78810338bcbe585bbca2741751
ingvarorn87 <ingvarorn87@gmail.com>
2018-09-04 15:26:36 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import {AlbumsModule} from './albums/albums.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    AppRoutingModule,
+    AlbumsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
